13d89d22c6d640112b86f470379b04af1d065d7b
Mats Högberg <mats@hgbrg.se>
2014-08-25 16:28:26 +0200

    Lade till egna funktioner för connect och disconnect

--- chat.py
+++ chat.py
@@ -28,18 +28,33 @@ def send(port):
 
             if args[0] == '/connect':
                 receive_port = args[1]
-
-                receive_processes[receive_port] = Process(target=receive, args=(receive_port,))
-                receive_processes[receive_port].daemon = True
-                receive_processes[receive_port].start()
+                connect(receive_processes, receive_port)
             elif args[0] == '/disconnect':
                 disconnect_port = args[1]
-                receive_processes[disconnect_port].terminate()
-                print('disconnected')
+                disconnect(receive_processes, disconnect_port)
             elif args[0] == '/help':
                 pass
         else:
             socket.send('%s %s' % (port, send_msg))
+
+
+def connect(processes, port):
+    if port not in processes:
+        process = Process(target=receive, args=(port,))
+        process.daemon = True
+        process.start()
+        processes[port] = process
+    else:
+        print 'already connected'
+
+
+def disconnect(processes, port):
+    if port in processes:
+        processes[port].terminate()
+        del processes[port]
+        print 'disconnected'
+    else:
+        print 'not connected'
 
 
 def receive(port):
